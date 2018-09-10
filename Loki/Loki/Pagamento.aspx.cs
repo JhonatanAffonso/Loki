c11b1ba045e72cffcc962ff043bdde5f688de893
JhoowEduardo <36669529+JhoowEduardo@users.noreply.github.com>
2018-09-10 20:17:38 -0300

    Consertando Pagamento

--- Loki/Loki/Pagamento.aspx.cs
+++ Loki/Loki/Pagamento.aspx.cs
@@ -8,29 +8,13 @@ using System.Web.UI.WebControls;
 
 namespace Loki
 {
-    public partial class Pagamento : System.Web.UI.Page
+    public partial class PagamentoDois : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        SqlConnection con = new SqlConnection("Integrated Security = SSPI; Persist Security Info=False;Initial Catalog = loki; Data Source =.");
-
-        protected void btnEnviar_Click(object sender, EventArgs e)
-        {
-            Response.Write("<script language='javascript'>alert('Ae Carai');</script>");
-            //SqlCommand command = new SqlCommand();
-        }
-
-        protected void btnLimpar_Click(object sender, EventArgs e)
-        {
-            txtNumCartao.Text = "";
-            txtNome.Text = "";
-            txtValidade.Text = "";
-            txtCodSeguranca.Text = "";
-        }
-
-
+        SqlConnection con = new SqlConnection();
     }
 }
